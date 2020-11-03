f1a321913000786df8fc0fb68effa4bc6f1b1010
ken0426 <kenkaitixyou0426@gmail.com>
2020-11-03 19:29:17 +0900

    ユーザーモデル作成

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # validates :nickname, true
+  # validates :email, true
+  # validates :encrypted_password, true
+  # validates :fastname, true
+  # validates :secondname, true
+  # validates :fastname_kana, true
+  # validates :secondname_kana, true
+  # validates :birthday, true
+
+  # has_many :items
+  # has_many :orders
+end
