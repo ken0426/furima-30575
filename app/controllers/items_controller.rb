babfc3327b0d3b5a8d021d3a09de9945a241e7a1
ken0426 <kenkaitixyou0426@gmail.com>
2020-11-12 12:18:33 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id == @item.user_id
+    if current_user.id == @item.user_id && @item.order == nil
       render :edit 
-    else @item.order != nil
+    else
        redirect_to root_path
     end
   end
